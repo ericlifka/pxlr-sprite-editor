b49dd3d3a69520c636df03db2ef4c9ed5616460f
Eric Lifka <eric.lifka@inin.com>
2017-04-03 21:53:22 -0400

    passing down a dimension and turning it into an array to be rendered as a list of buttons

--- src/ui/components/pixel-grid/component.ts
+++ src/ui/components/pixel-grid/component.ts
@@ -1,4 +1,12 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class PixelGrid extends Component {
+  @tracked('args.width')
+  get buttons() {
+    let buttons = [];
+    for (let i = 0; i < this.args.width; i++) {
+      buttons.push(i);
+    }
+    return buttons;
+  }
 };
