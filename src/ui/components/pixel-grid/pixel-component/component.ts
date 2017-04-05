63a1439731029886501e71df1897109214d8930d
Eric Lifka <eric.lifka@inin.com>
2017-04-04 21:04:59 -0400

    rage starting over

--- src/ui/components/pixel-grid/pixel-component/component.ts
+++
@@ -1,7 +0,0 @@
-import Component, { tracked } from '@glimmer/component';
-
-export default class PixelComponent extends Component {
-  click() {
-    this.args.clickPixel(this.args.pixel);
-  }
-}
--- src/ui/components/pxlr-sprite-editor/component.ts
+++ src/ui/components/pxlr-sprite-editor/component.ts
@@ -1,16 +1,5 @@
 import Component, { tracked } from "@glimmer/component";
 
 export default class PxlrSpriteEditor extends Component {
-  @tracked width = 8;
-  @tracked height = 4;
 
-  widthChange(event) {
-    let val = event.target.value;
-    this.width = parseInt(val, 10);
-  }
-
-  heightChange(event) {
-    let val = event.target.value;
-    this.height = parseInt(val, 10);
-  }
 }
