5ea2b3ec7b6e0aa902118852ee0178fc8dffbc6e
Eric Lifka <eric.lifka@inin.com>
2017-04-05 00:04:11 -0400

    tie controls into updating sprite dimensions

--- src/ui/components/pxlr-sprite-editor/component.ts
+++ src/ui/components/pxlr-sprite-editor/component.ts
@@ -1,17 +1,19 @@
-import Component, { tracked } from "@glimmer/component";
+import Component, {tracked} from "@glimmer/component";
 
 class Pixel {
   id: number = Date.now();
   @tracked color: string;
 
-  constructor(color="#ffffff") {
+  constructor(color = "#ffffff") {
     this.color = color;
   }
 }
 
 export default class PxlrSpriteEditor extends Component {
+  @tracked width: number = 16;
+  @tracked height: number = 16;
   @tracked activeColor: string = "green";
-  @tracked pixels: Pixel[][] = [ [ new Pixel() ] ];
+  @tracked pixels: Pixel[][] = [[new Pixel()]];
 
   didInsertElement() {
     this.activeColor = "#" + this.element.getElementsByClassName('jscolor')[0].value;
@@ -26,16 +28,28 @@ export default class PxlrSpriteEditor extends Component {
   }
 
   addRow() {
-    // let pixel = new Pixel();
-    // this.pixels = [ ...this.pixels, pixel ];
     let row = [];
     for (let i = 0; i < this.pixels[0].length; i++) {
       row.push(new Pixel());
     }
-    this.pixels = [ ...this.pixels, row ]
+    this.pixels = [...this.pixels, row]
   }
 
   addColumn() {
-    this.pixels = this.pixels.map(row => [ ...row, new Pixel() ]);
+    this.pixels = this.pixels.map(row => [...row, new Pixel()]);
+  }
+
+  updateDimensions(dimension, event) {
+    this[dimension] = parseInt(event.target.value, 10);
+  }
+
+  updateSpriteDimensions() {
+    while (this.width > this.pixels[0].length) {
+      this.addColumn();
+    }
+
+    while (this.height > this.pixels.length) {
+      this.addRow();
+    }
   }
 }
