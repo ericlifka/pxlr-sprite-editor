f8b7676193f6bddb699b14c38dc2f63c60b23fe8
Eric Lifka <eric.lifka@inin.com>
2017-04-04 21:42:36 -0400

    simpler version but working much better, no idea what's really different

--- src/ui/components/pxlr-sprite-editor/component.ts
+++ src/ui/components/pxlr-sprite-editor/component.ts
@@ -1,5 +1,28 @@
 import Component, { tracked } from "@glimmer/component";
 
-export default class PxlrSpriteEditor extends Component {
+class Pixel {
+  id: number = Date.now();
+  @tracked color: string;
 
+  constructor(color="#00ff00") {
+    this.color = color;
+  }
+}
+
+export default class PxlrSpriteEditor extends Component {
+  @tracked pixels: Pixel[] = [ new Pixel() ];
+
+  changeColor(color) {
+    // this.pixels[0].color = color;
+    this.pixels.forEach(pixel => pixel.color = color);
+  }
+
+  clickPixel(pixel) {
+    pixel.color = "#ffffff";
+  }
+
+  addPixel() {
+    let pixel = new Pixel();
+    this.pixels = [ ...this.pixels, pixel ];
+  }
 }
