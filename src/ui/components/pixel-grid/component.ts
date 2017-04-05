0d4b2b90ecb580b112a3fb04bf4457b1f230ef21
Eric Lifka <eric.lifka@inin.com>
2017-04-04 21:02:33 -0400

    attempt to use classes to fix binding issues, nothing works

--- src/ui/components/pixel-grid/component.ts
+++ src/ui/components/pixel-grid/component.ts
@@ -1,33 +1,56 @@
 import Component, { tracked } from '@glimmer/component';
 
+class Sprite {
+  @tracked width;
+  @tracked height;
+  @tracked rows;
+
+  constructor(width=16, height=16) {
+    this.width = width;
+    this.height = height;
+    this.rows = [];
+  }
+}
+
+class PixelRow {
+  @tracked pixels;
+
+  constructor() {
+    this.pixels = [];
+  }
+}
+
+class Pixel {
+  @tracked color;
+
+  constructor(color="#ffffff") {
+    this.color = color;
+  }
+}
+
 export default class PixelGrid extends Component {
   @tracked sprite = this.updateSprite();
 
   updateSprite() {
-    let sprite = {
-      width: this.args.width || 8,
-      height: this.args.height || 8,
-      pixels: []
-    };
+    let sprite = new Sprite(this.args.width, this.args.height);
 
     for (let y = 0; y < sprite.height; y++) {
-      let row = [];
+      let row = new PixelRow();
 
       for (let x = 0; x < sprite.width; x++) {
-        let pixel = {
-          x, y, color: "#ffffff"
-        };
-        row.push(pixel);
+        let pixel = new Pixel();
+        row.pixels.push(pixel);
       }
 
-      sprite.pixels.push(row);
+      sprite.rows.push(row);
     }
 
     return sprite;
   }
 
-  clickPixel(x, y) {
-    this.sprite.pixels[y][x].color = "blue";
+  clickPixel(pixel) {
+    pixel.color = "blue";
+    window.pixel = pixel;
   }
 
   didUpdate() {
--- src/ui/components/pixel-grid/pixel-component/component.ts
+++ src/ui/components/pixel-grid/pixel-component/component.ts
@@ -1,10 +1,7 @@
 import Component, { tracked } from '@glimmer/component';
 
 export default class PixelComponent extends Component {
-  @tracked backgroundColor = this.args.color;
-
   click() {
-    this.backgroundColor = "blue";
-    this.args.clickPixel(this.args.x, this.args.y);
+    this.args.clickPixel(this.args.pixel);
   }
 }
