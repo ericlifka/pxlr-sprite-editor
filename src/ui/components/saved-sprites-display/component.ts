5c957fc09e61e958e862d53df8c3e8fc3d126c0a
Eric Lifka <eric.lifka@inin.com>
2017-04-21 23:51:41 -0400

    show mini representations of saved sprites in the load buttons

--- src/ui/components/saved-sprites-display/component.ts
+++ src/ui/components/saved-sprites-display/component.ts
@@ -1,6 +1,10 @@
 import Component, {tracked} from "@glimmer/component";
 import Store from "../store";
 
-export default class PxlrSpriteEditor extends Component {
+export default class SavedSpritesDisplay extends Component {
   @tracked store: Store = Store.getStore();
+
+  openSprite(sprite) {
+    this.store.openSprite(sprite);
+  }
 }
