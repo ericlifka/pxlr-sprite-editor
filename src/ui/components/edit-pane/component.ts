3ffe3798d72d00b1715723e32901ed741eace5a8
Eric Lifka <eric.lifka@inin.com>
2017-04-21 22:10:50 -0400

    split the pixel grid from the rest of the edit controls so it stands alone as a way of rendering a sprite

---
+++ src/ui/components/edit-pane/component.ts
@@ -0,0 +1,31 @@
+import Component, {tracked} from "@glimmer/component";
+import Store from "../store";
+import Pixel from "../pixel";
+
+export default class EditPane extends Component {
+  @tracked store: Store = Store.getStore();
+
+  @tracked activeColor: string;
+
+  didInsertElement() {
+    let inputEle = this.element.getElementsByClassName('jscolor')[0];
+    window['jscolor'](inputEle);
+    this.activeColor = "#" + inputEle.value;
+  }
+
+  colorChange(event) {
+    this.activeColor = "#" + event.target.value;
+  }
+
+  selectPixel(pixel: Pixel) {
+    this.store.changePixelColor(pixel, this.activeColor);
+  }
+
+  cancelSprite() {
+    this.store.closeSprite();
+  }
+
+  onToggle() {
+    this.store.toggleWhiteAsEmpty();
+  }
+}
--- src/ui/components/pixel-grid/component.ts
+++ src/ui/components/pixel-grid/component.ts
@@ -1,28 +1,16 @@
 import Component, {tracked} from "@glimmer/component";
-import Store from "../store";
 
 export default class PixelGrid extends Component {
-  @tracked activeColor: string;
   @tracked mouseDown: boolean = false;
 
-  @tracked store: Store = Store.getStore();
-
   didInsertElement() {
-    let inputEle = this.element.getElementsByClassName('jscolor')[0];
-    window['jscolor'](inputEle);
-    this.activeColor = "#" + inputEle.value;
-
     document.addEventListener('mouseup', () => {
       this.mouseDown = false
     });
   }
 
-  colorChange(event) {
-    this.activeColor = "#" + event.target.value;
-  }
-
   clickPixel(pixel) {
-    this.store.changePixelColor(pixel, this.activeColor);
+    this.args.selectPixel(pixel);
   }
 
   onMouseDown(pixel) {
@@ -33,13 +21,5 @@ export default class PixelGrid extends Component {
     if (this.mouseDown) {
       this.clickPixel(pixel);
     }
-  }
-
-  cancelSprite() {
-    this.store.closeSprite();
-  }
-
-  onToggle() {
-    this.store.toggleWhiteAsEmpty();
   }
 }
