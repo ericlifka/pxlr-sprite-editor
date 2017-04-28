86782a9997470213ab2fa6f6020bc4c0a0781d3a
Eric Lifka <eric.lifka@inin.com>
2017-04-27 22:17:06 -0400

    cleanup and add clarification around migrations

--- src/ui/components/sprite.ts
+++ src/ui/components/sprite.ts
@@ -5,88 +5,118 @@ type Row = Pixel[];
 type Frame = Row[];
 
 export default class Sprite {
-  @tracked frames: Frame[];
-  @tracked firstFrame: Frame;
   @tracked name: string;
+  @tracked whiteAsEmpty: boolean;
   @tracked width: number;
   @tracked height: number;
-  @tracked whiteAsEmpty: boolean = true;
+  @tracked frames: Frame[];
+  @tracked firstFrame: Frame;
 
-  static currentSchemaVer: number = 2;
+  static CURRENT_SCHEMA_VERSION: number = 2;
 
-  save() {
-    localStorage[this.name] = JSON.stringify({
-      schema: Sprite.currentSchemaVer,
-      name: this.name,
-      width: this.width,
-      height: this.height,
-      whiteAsEmpty: this.whiteAsEmpty,
-      frames: this.frames.map((frame: Frame) =>
-        frame.map((row: Row) =>
-          row.map((pixel: Pixel) =>
-            pixel.color.toLowerCase())))
-    });
-  }
-
-  getBlob() {
+  getBlob(): string {
     let whiteAsEmpty = this.whiteAsEmpty;
+    let json = this.toJSON();
 
-    return JSON.stringify(
-      this.frames.map(frame => frame.map(row => row.map(pixel =>
-        whiteAsEmpty && pixel.color.toLowerCase() === "#ffffff" ? null : pixel.color))));
+    if (whiteAsEmpty) {
+      json['frames'] = json['frames'].map(frame => frame.map(row => row.map(color =>
+        color === "#ffffff" ? null : color)));
+    }
+
+    return JSON.stringify(json);
   }
 
-  toggleWhiteAsEmpty() {
+  toggleWhiteAsEmpty(): void {
     this.whiteAsEmpty = !this.whiteAsEmpty;
     this.save();
   }
 
-  static initializeEmptySprite(name, width, height) {
-    let sprite = new Sprite();
-    sprite.name = name;
-    sprite.width = width;
-    sprite.height = height;
-    let frame: Frame = [];
-    for (let h = 0; h < height; h++) {
-      let row: Row = [];
-      for (let w = 0; w < width; w++) {
-        row.push(new Pixel());
-      }
-      frame.push(row);
-    }
-    sprite.frames = [ frame ];
-    sprite.firstFrame = frame;
-    sprite.save();
-
-    return sprite;
+  /**
+   * WARNING - WARNING - WARNING:
+   *  The following four functions (save, toJSON, load, initializeEmptySprite) are responsible for moving data between
+   *  localStorage and Sprite models.
+   *
+   *  THEY SHOULD ALL BE REFERENCING THE SAME PROPERTIES.
+   *
+   *  If they aren't all in sync then things are going to go bad in one direction or the other.
+   *  If changes are needed to one then do the following:
+   *    1) Update the other two to match
+   *    2) Bump the CURRENT_SCHEMA_VERSION property
+   *    3) Add a migration from the old schema value to the new schema value
+   *    4) Add the migration to the migration list in `runMigrations` below
+   */
+  save(): void {
+    localStorage[this.name] = JSON.stringify(this.toJSON());
   }
 
-  static load(name: string) {
+  private toJSON(): object {
+    return {
+      schema: Sprite.CURRENT_SCHEMA_VERSION,
+      name: this.name,
+      whiteAsEmpty: this.whiteAsEmpty,
+      width: this.width,
+      height: this.height,
+      frames: this.frames.map((frame: Frame) =>
+        frame.map((row: Row) =>
+          row.map((pixel: Pixel) =>
+            pixel.color.toLowerCase())))
+    };
+  }
+
+  static load(name: string): Sprite {
     let sprite = new Sprite();
     let descriptor = JSON.parse(localStorage[name]);
 
-    descriptor = Sprite.runMigrations(descriptor, name);
+    descriptor = runMigrations(descriptor, name);
 
     sprite.name = descriptor.name;
     sprite.whiteAsEmpty = descriptor.whiteAsEmpty;
-    sprite.height = descriptor.height;
     sprite.width = descriptor.width;
+    sprite.height = descriptor.height;
     sprite.frames = descriptor.frames.map(frame => frame.map(row => row.map(color => new Pixel(color))));
     sprite.firstFrame = sprite.frames[0];
 
     return sprite;
   }
 
-  private static runMigrations(json: any, name: string) {
-    let schema = json.schema || 0;
-    switch(schema) {
+  static initializeEmptySprite(name: string, width: number, height: number): Sprite {
+    let sprite = new Sprite();
 
-      case 0: json = migrate0to1(json, name);
-      case 1: json = migrate1to2(json);
-      case 2:
-        // current
-        return json;
+    sprite.name = name;
+    sprite.whiteAsEmpty = true;
+    sprite.width = width;
+    sprite.height = height;
+    sprite.frames = createBlankFrameList(width, height);
+    sprite.firstFrame = sprite.frames[0];
+    sprite.save();
+
+    return sprite;
+  }
+}
+
+function createBlankFrameList(width: number, height: number) {
+  let frame: Frame = [];
+  for (let h = 0; h < height; h++) {
+    let row: Row = [];
+    for (let w = 0; w < width; w++) {
+      row.push(new Pixel());
     }
+    frame.push(row);
+  }
+
+  return [ frame ];
+}
+
+function runMigrations(json: any, name: string) {
+  let schema = json.schema || 0;
+
+  // Jump into the list of migrations at the current level of the model, and start up-converting from there.
+  switch(schema) {
+    case 0: json = migrate0to1(json, name);
+    case 1: json = migrate1to2(json);
+    case 2:
+      // current schema version
+      return json;
   }
 }
 
