9114762dc29dac5b8088d524b0c3d8bc02bf2f86
Eric Lifka <eric.lifka@inin.com>
2017-04-29 23:58:21 -0400

    start and stop animation functions

--- src/ui/components/edit-pane/component.ts
+++ src/ui/components/edit-pane/component.ts
@@ -9,6 +9,9 @@ export default class EditPane extends Component {
   @tracked activeColor: string;
   @tracked copiedFrame: Frame;
   @tracked displaySize: string = "medium";
+  @tracked animationClass: string = "";
+
+  animationId: number;
 
   didInsertElement() {
     let jscolor = window['jscolor'];
@@ -62,5 +65,59 @@ export default class EditPane extends Component {
   removeFrame(frame: Frame) {
     this.copyFrame(frame);
     this.store.removeFrameFromSprite(this.args.sprite, frame);
+  }
+
+  showAnimation() {
+    this.animationClass = "show-animation";
+
+    let root: HTMLElement = this.element as HTMLElement;
+    let frames = root.getElementsByClassName('frame');
+
+    let activeFrame = 0;
+    let boundary = frames.length;
+    let start;
+
+    function updateClasses() {
+      for (let i = 0; i < boundary; i++) {
+        if (i === activeFrame) {
+          frames[i].classList.remove('hidden');
+        } else {
+          frames[i].classList.add('hidden');
+        }
+      }
+    }
+
+    function updateCounter() {
+      activeFrame++;
+      if (activeFrame >= boundary) {
+        activeFrame = 0;
+      }
+    }
+
+    function frameHandler(time) {
+      if (!start) {
+        start = time;
+      }
+
+      if (time - start > 250) {
+        start = time;
+
+        updateClasses();
+        updateCounter();
+      }
+
+      this.animationId = requestAnimationFrame(frameHandler);
+    }
+    this.animationId = requestAnimationFrame(frameHandler);
+  }
+
+  hideAnimation() {
+    cancelAnimationFrame(this.animationId);
+    this.animationClass = "";
+    let root: HTMLElement = this.element as HTMLElement;
+    let frames = root.getElementsByClassName('frame');
+    for (let i = 0; i < frames.length; i++) {
+      frames[i].classList.remove('hidden');
+    }
   }
 }
