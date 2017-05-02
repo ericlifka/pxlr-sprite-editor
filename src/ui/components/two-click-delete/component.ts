d4ac91993881033fd37c1b4503dbd5b5c4b3507f
Eric Lifka <eric.lifka@inin.com>
2017-05-01 22:59:30 -0400

    add a third step for snazziness

--- src/ui/components/two-click-delete/component.ts
+++ src/ui/components/two-click-delete/component.ts
@@ -2,13 +2,18 @@ import Component, {tracked} from "@glimmer/component";
 
 export default class TwoClickDelete extends Component {
   @tracked showPrompt: boolean = false;
+  @tracked showCheck: boolean = false;
 
   click() {
     if (!this.showPrompt) {
       this.showPrompt = true;
     } else {
-      this.showPrompt = false;
-      this.args.action();
+      this.showCheck = true;
+      setTimeout(() => {
+        this.showPrompt = false;
+        this.showCheck = false;
+        this.args.action();
+      }, 400);
     }
   }
 }
